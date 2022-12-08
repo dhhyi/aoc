dca2f93c5462c15c091257b695ef8b2dd4836aea
Danilo Hoffmann <dhhyi@aol.com>
2022-12-08 18:27:51 +0100

    create script

---
+++ create.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -e
+
+day="$(date +%-d)"
+day="${2:-$day}"
+
+mkdir -p "day$day"
+dcc "dcc://${1:-javascript}" "day$day" --name "AOC 2022 Day $day" --no-vscode
+
+cat <<EOF > "day$day/day$day.js"
+const stdin = process.openStdin();
+let data = "";
+stdin.on("data", function (chunk) {
+    data += chunk;
+});
+
+stdin.on("end", function () {
+    console.log(data);
+});
+EOF
+
+code-insiders "day$day"
