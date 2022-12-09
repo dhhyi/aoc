0ee0af808b136770c55dc0b8e13fc7072333732d
Danilo Hoffmann <dhhyi@aol.com>
2022-12-09 11:21:04 +0100

    exit on error

--- create.sh
+++ create.sh
@@ -1,9 +1,11 @@
-#!/bin/sh -e
+#!/bin/sh
+
+set -e
 
 day="$(date +%-d)"
 day="${2:-$day}"
 
-mkdir -p "day$day"
+# mkdir -p "day$day"
 dcc "dcc://${1:-javascript}" "day$day" --name "AOC 2022 Day $day" --no-vscode
 
 cat <<EOF > "day$day/day$day.js"
